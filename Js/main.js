83d56bfef1fd8d51a1dac0b37c4d3d93319e5f9b
AleElordi <amelordi@gmail.com>
2021-10-01 00:17:19 -0300

    Local storage
    Agregamos el local storage funcionando

--- Js/main.js
+++ Js/main.js
@@ -14,13 +14,14 @@ button.addEventListener("click", ()=> {
 function buscarClima(ciudadABuscar){
     console.log("Valor buscado por usuario ", ciudadABuscar);
     fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${ciudadABuscar}&appid=${API_KEY}&units=metric&lang={sp, es}`
+      `https://api.openweathermap.org/data/2.5/weather?q=${ciudadABuscar}&appid=${API_KEY}&units=metric&lang=es`
     ).then(function(response){
         return response.json();
     }).then(function(data){
         console.log('Imprimo Json', data);
         mostrarClima(data);
         datosImg(data);
+        saveResults('responsewt', data);
     })
     .catch(function(error){
         console.log('Fallo!', error);
@@ -30,43 +31,54 @@ function buscarClima(ciudadABuscar){
 
 const mostrarClima = (data) => {
 
-//Busqueda de ID
-const ciudadElegida = document.getElementById('ciudad');
-const temperaturaGrados = document.getElementById('temperatura');
-const temperaturaTermica = document.getElementById('termica');
-const humedadCiudad = document.getElementById('humedad');
-const temperaturaMaxima = document.getElementById('temperaturaMaxima');
-const temperaturaMinima = document.getElementById('temperaturaMinima');
-const velocidadViento = document.getElementById('vientoVel');
-const climaEstado = document.getElementById('estadoclima');
-const presionAtm = document.getElementById('presioatm');
+    //Busqueda de ID
+    const ciudadElegida = document.getElementById('ciudad');
+    const temperaturaGrados = document.getElementById('temperatura');
+    const temperaturaTermica = document.getElementById('termica');
+    const humedadCiudad = document.getElementById('humedad');
+    const temperaturaMaxima = document.getElementById('temperaturaMaxima');
+    const temperaturaMinima = document.getElementById('temperaturaMinima');
+    const velocidadViento = document.getElementById('vientoVel');
+    const climaEstado = document.getElementById('estadoclima');
+    const presionAtm = document.getElementById('presioatm');
 
+    //Datos en HTML
+    ciudadElegida.innerHTML = data.name;
+    temperaturaGrados.innerHTML = data.main.temp;
+    temperaturaTermica.innerHTML = data.main.feels_like;
+    humedadCiudad.innerHTML = data.main.humidity;
+    temperaturaMinima.innerHTML = data.main.temp_min;
+    temperaturaMaxima.innerHTML = data.main.temp_max;
+    velocidadViento.innerHTML = data.wind.speed;
+    presionAtm.innerHTML = data.main.pressure;
 
-//Datos de HTML
-ciudadElegida.innerHTML = data.name;
-temperaturaGrados.innerHTML = data.main.temp;
-temperaturaTermica.innerHTML = data.main.feels_like;
-humedadCiudad.innerHTML = data.main.humidity;
-temperaturaMinima.innerHTML = data.main.temp_min;
-temperaturaMaxima.innerHTML = data.main.temp_max;
-velocidadViento.innerHTML = data.wind.speed;
-presionAtm.innerHTML = data.main.pressure;
-
-let datosClima = data.weather;
-    datosClima.map( item => { 
-    return climaEstado.innerHTML = item.description;
-});
-
+    //Datos del tiempo para HTMl
+    let datosClima = data.weather;
+        datosClima.map( item => { 
+        return climaEstado.innerHTML = item.description;
+    });
 };
 
-//imagen dinamica de clima
+
+//--------------------------imagen dinamica de clima------------------------------//
 const datosImg = (data) => {
 
 const imgClima = data.weather.map( item => { return item.main; });
 const climaImagen = document.getElementById('climaImagen');
 
-
 climaImagen.setAttribute('src', `imgs/${imgClima}.png`);
 
 }
 
+//----------------Guardado en local Storage y autocompleto-------------------------//
+
+function saveResults(responsewt, data){    
+//Guardo en un objeto JSON
+localStorage.setItem(responsewt, JSON.stringify(data))
+}
+    
+//Get para el local storage
+const busquedaclima = JSON.parse(localStorage.getItem('responsewt'));
+if (buscarClima != null){
+    mostrarClima(busquedaclima);
+}
